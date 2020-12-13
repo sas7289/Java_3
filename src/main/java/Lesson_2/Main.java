3adc04d0d22256f5fa90290c17e4dc00d6010c95
Alex Shakhov <sas7289@yandex.ru>
2020-12-13 22:55:55 +0300

    cosmetic

--- src/main/java/Lesson_2/Main.java
+++ src/main/java/Lesson_2/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             connection();
+            Queries.connection();
         } catch (ClassNotFoundException |SQLException e) {
             e.printStackTrace();
         }
@@ -19,6 +20,28 @@ public class Main {
             createTable();
             truncateTable();
             floodTable();
+
+            try {
+                connection();
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Queries.getCoast("product1");
+                Queries.setCoast("product1", 1111);
+                Queries.getCoast("product1");
+                Queries.getProducts(5000, 10000);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+            try {
+                disconnect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -31,21 +54,10 @@ public class Main {
         }
     }
 
-    private static void floodTable() throws SQLException {
-        long begin = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
-
-            stmt.addBatch(String.format("INSERT INTO products (good_name, good_price) " +
-                    "values ('product%s', %d)", i, i*1111 + i*15/7));
-        }
-        connection.setAutoCommit(false);
-        stmt.executeBatch();
-        connection.setAutoCommit(true);
-        System.out.println(System.currentTimeMillis() - begin);
-    }
-
-    private static void truncateTable() throws SQLException {
-        stmt.executeUpdate("DELETE FROM products");
+    private static void connection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/mainDB.db");
+        stmt = connection.createStatement();
     }
 
     private static void createTable() throws SQLException {
@@ -56,10 +68,19 @@ public class Main {
                 "good_price INT);");
     }
 
-    private static void connection() throws ClassNotFoundException, SQLException {
-        Class.forName("org.sqlite.JDBC");
-        connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/mainDB.db");
-        stmt = connection.createStatement();
+    private static void truncateTable() throws SQLException {
+        stmt.executeUpdate("DELETE FROM products");
+    }
+
+    private static void floodTable() throws SQLException {
+        for (int i = 0; i < 10000; i++) {
+
+            stmt.addBatch(String.format("INSERT INTO products (good_name, good_price) " +
+                    "values ('product%s', %d)", i, i*1111 + i*15/7));
+        }
+        connection.setAutoCommit(false);
+        stmt.executeBatch();
+        connection.setAutoCommit(true);
     }
 
     private static void disconnect() throws SQLException {
--- src/main/java/Lesson_2/Queries.java
+++ src/main/java/Lesson_2/Queries.java
@@ -6,57 +6,41 @@ public class Queries {
     private static Connection connection;
     private static Statement stmt;
     private static ResultSet rs;
+    private static String dbUrl;
 
 
-    public static void main(String[] args) {
-        try {
-            connection();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            getCoast("product1");
-            setCoast("product1", 1111);
-            getCoast("product1");
-            getProducts(5000, 10000);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
 
 
-        try {
-            disconnect();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+
+
+    public static void setBaseUrl(String url) {
+        dbUrl = url;
+    }
+
+    public static void getCoast(String nameProduct) throws SQLException {
+        rs = stmt.executeQuery(String.format("SELECT good_price FROM products WHERE good_name = '%s'", nameProduct));
+
+        String result = rs.next() ? String.valueOf(rs.getInt("good_price")) : "Такого товара нет";
+        System.out.println(result);
 
     }
 
-    private static void getProducts(int begin, int end) throws SQLException {
+    public static void setCoast(String nameProduct, int coast) throws SQLException {
+        stmt.executeUpdate(String.format("UPDATE products SET good_price = %d WHERE good_name = '%s'", coast, nameProduct));
+    }
+
+    public static void getProducts(int begin, int end) throws SQLException {
         rs = stmt.executeQuery(String.format("SELECT * FROM products WHERE good_price BETWEEN %d AND %d", begin, end));
         while (rs.next()) {
             System.out.printf("%s %d%n", rs.getString("good_name"), rs.getInt("good_price"));
         }
     }
 
-    private static void setCoast(String nameProduct, int coast) throws SQLException {
-        stmt.executeUpdate(String.format("UPDATE products SET good_price = %d WHERE good_name = '%s'", coast, nameProduct));
-    }
-
-    private static void disconnect() throws SQLException {
+    public static void disconnect() throws SQLException {
         stmt.close();
     }
 
-    private static void getCoast(String nameProduct) throws SQLException {
-        rs = stmt.executeQuery(String.format("SELECT good_price FROM products WHERE good_name = '%s'", nameProduct));
-
-        String result = rs.next() ? String.valueOf(rs.getInt("good_price") + "\n") : "Такого товара нет\n";
-        System.out.printf(result);
-
-    }
-
-    private static void connection() throws ClassNotFoundException, SQLException {
+    public static void connection() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/mainDB.db");
         stmt = connection.createStatement();
